3cac1df09d0244ec8e9d3a3e4019e73f76afca35
Richard Lipa <rlipac31@gmail.com>
2017-06-20 17:58:38 -0500

    provando video

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+
+
+$('[data-toggle-dia]').click(function (ev){
+    const panel= $(this).data('toggleDia')
+    $('#lineup-tabs').foundation('selectTab', panel) /* cambia los paneles sabado y domingo y viernes*/
+})
+
+const $offCanvas = $('#offCanvas')
+$offCanvas.find('li').click(function (ev){
+$offCanvas.foundation('close') 
+    
+})
